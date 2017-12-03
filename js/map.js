d03fe8675816ded5ae6f0df4638f846cf14fb614
Dmitry <bens1iller@gmail.com>
2017-12-03 22:48:41 +0300

    Правка
    количество циклов из функции, перевод типа отеля на русский, подключил
    map.js, записал массивы данных столбиком.

--- js/map.js
+++ js/map.js
@@ -4,32 +4,66 @@ var mapFilter = mapElement.querySelector('.map__filters-container');
 var similarListButtons = document.querySelector('.map__pins');
 var similarNoticeButton = document.querySelector('template').content.querySelector('.map__pin');
 var similarNoticeCard = document.querySelector('template').content.querySelector('article.map__card');
+
 // 1 Пункт
 // Массивы констант
 
-var AVATAR = ['img/avatars/user{{01}}.png]', 'img/avatars/user{{02}}.png]', 'img/avatars/user{{03}}.png]', 'img/avatars/user{{04}}.png]', 'img/avatars/user{{05}}.png]', 'img/avatars/user{{06}}.png]', 'img/avatars/user{{07}}.png]', 'img/avatars/user{{08}}.png'];
-var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var TYPE = ['flat', 'house', 'bungalo'];
-var CHECKIN = ['12:00', '13:00', '14:00'];
-var CHECKOUT = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var NUMBER = 8;
+var AVATAR = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08'];
+
+var TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'];
+
+var TYPE = [
+  'flat',
+  'house',
+  'bungalo'];
+
+var CHECK = [
+  '12:00',
+  '13:00',
+  '14:00'];
+
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'];
+
+var photos = [];
 var notices = [];
 
 // Нахождение случайного числа
 
-var getRandom = function (endIndex, startIndex) {
-  return Math.floor(startIndex + Math.random() * (endIndex - startIndex + 1));
+var getRandom = function (startIndex, endIndex) {
+  var randomNum = Math.floor(Math.random() * (endIndex - startIndex + 1)) + startIndex;
+  return randomNum;
 };
 
-// Создание нового массива перебором значений старого массива в случайном порядки
+// Создание нового массива перебором значений старого массива в случайном порядке
 
 var getRandomArr = function (arr) {
   var spliceArr = [];
   var copyArr = arr.slice();
   while (copyArr.length > 0) {
-    var randomArr = getRandom(copyArr.length - 1, 0);
-    var current = copyArr.splice(randomArr, 1)[0];
+    var RandomItem = getRandom(0, copyArr.length - 1);
+    var current = copyArr.splice(RandomItem, 1)[0];
     spliceArr.push(current);
   }
   return spliceArr;
@@ -38,28 +72,29 @@ var getRandomArr = function (arr) {
 // Создание массива с данными по заданию
 
 var getNotice = function () {
+  var avatarArr = getRandomArr(AVATAR);
   var titleArr = getRandomArr(TITLES);
+  var NUMBER = titleArr.length;
 
   for (var i = 0; i < NUMBER; i++) {
-    var locationX = getRandom(900, 300);
-    var locationY = getRandom(500, 100);
+    var locationX = getRandom(300, 900);
+    var locationY = getRandom(100, 500);
 
     var notice = {
       author: {
-        avatar: AVATAR[i]
+        avatar: 'img/avatars/user' + avatarArr[i] + '.png'
       },
       offer: {
         title: titleArr[i],
         address: locationX + ', ' + locationY,
-        price: getRandom(1000000, 1000),
+        price: getRandom(1000, 1000000),
         type: TYPE[getRandom(0, TYPE.length - 1)],
-        rooms: getRandom(5, 1),
-        guests: rooms * 2,
-        checkin: CHECKIN[getRandom(CHECKIN.length - 1, 0)],
-        checkout: CHECKOUT[getRandom(CHECKOUT.length - 1, 0)],
-        FEATURES: getRandomArr(FEATURES).slice(0, getRandom(FEATURES.length - 1, 0)),
+        rooms: getRandom(1, 5),
+        checkin: CHECK[getRandom(0, CHECK.length - 1)],
+        checkout: CHECK[getRandom(0, CHECK.length - 1)],
+        features: getRandomArr(FEATURES).slice(0, getRandom(0, FEATURES.length - 1)),
         description: '',
-        photos: ''
+        photos: photos
       },
       location: {
         x: locationX,
@@ -70,6 +105,16 @@ var getNotice = function () {
   }
 };
 getNotice();
+
+
+var cleanFeatureList = function () {
+  var popupFeatureList = document.querySelector('template').content.querySelector('.popup__features');
+  while (popupFeatureList.firstChild) {
+    popupFeatureList.removeChild(popupFeatureList.firstChild);
+  }
+};
+
+cleanFeatureList();
 
 // 2 Пункт - показываем карточку
 
@@ -96,10 +141,22 @@ var renderNoticeCard = function (notice) {
   noticeElementCard.querySelector('p small').textContent = notice.offer.address;
   noticeElementCard.querySelector('.popup__price').textContent = notice.offer.price + '&#x20bd;/ночь';
   noticeElementCard.querySelector('h4').textContent = notice.offer.type;
-  noticeElementCard.querySelector('h4 + p').textContent = notice.offer.rooms + ' комнаты  для ' + notice.offer.guests + ' гостей';
+  noticeElementCard.querySelector('h4 + p').textContent = notice.offer.rooms + ' комнаты  для ' + (notice.offer.rooms * 2) + ' гостей';
   noticeElementCard.querySelector('h4 + p + p').textContent = 'Заезд после ' + notice.offer.checkin + ', выезд до ' + notice.offer.checkout;
   noticeElementCard.querySelector('.popup__avatar').removeAttribute('src');
   noticeElementCard.querySelector('.popup__avatar').setAttribute('src', notice.author.avatar);
+
+  switch (noticeElementCard.querySelector('h4').textContent) {
+    case 'flat':
+      noticeElementCard.querySelector('h4').textContent = 'квартира';
+      break;
+    case 'house':
+      noticeElementCard.querySelector('h4').textContent = 'дом';
+      break;
+    case 'bungalo':
+      noticeElementCard.querySelector('h4').textContent = 'бунгало';
+      break;
+  }
 
   var popupFeature = noticeElementCard.querySelector('.popup__features');
 
@@ -114,8 +171,6 @@ var renderNoticeCard = function (notice) {
 
   return noticeElementCard;
 };
-
-// 4 Пункт
 
 var fragment = document.createDocumentFragment();
 var fragmentCard = document.createDocumentFragment();
