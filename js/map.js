546e9a11c8390f8d1c7cbcff420e5a4260b743a5
Dmitry <bens1iller@gmail.com>
2017-12-15 21:50:19 +0300

    Валидация ввода данных пользователем

--- js/map.js
+++ js/map.js
@@ -64,6 +64,30 @@ var mapCardsNode;
 var noticeForm = document.querySelector('.notice__form');
 var noticeFormFieldsets = noticeForm.querySelectorAll('fieldset');
 
+var noticeFormTitleInput = noticeForm.querySelector('#title');
+var noticeFormAddressInput = noticeForm.querySelector('#address');
+var noticeFormPriceInput = noticeForm.querySelector('#price');
+var noticeFormTimeInSelect = noticeForm.querySelector('#timein');
+var noticeFormTimeOutSelect = noticeForm.querySelector('#timeout');
+var noticeFormTypeSelect = noticeForm.querySelector('#type');
+var noticeFormRoomstSelect = noticeForm.querySelector('#room_number');
+var noticeFormCapacitySelect = noticeForm.querySelector('#capacity');
+
+var TITLE_INPUT_MIN_LENGTH = 30;
+var TITLE_INPUT_MAX_LENGTH = 100;
+
+var PRICE_INPUT_MIN_DEFAULT = 0;
+
+var PRICE_INPUT_MIN_TYPES = {
+  'flat': 0,
+  'bungalo': 1000,
+  'house': 5000,
+  'palace': 1000000
+};
+
+var PRICE_INPUT_MAX_VALUE = 1000000;
+var priceInputMin = PRICE_INPUT_MIN_DEFAULT;
+
 
 //  Переключатели видимости узла
 
@@ -311,6 +335,7 @@ var onMapPinMainMouseUp = function () {
   mapElement.classList.remove('map--faded');
   mapGeneratedPins.forEach(showNode);
   enableNoticeForm();
+  initInputsSelectsAttributes();
 };
 
 //  Делегирование на всплытии
@@ -377,3 +402,85 @@ mapPins.addEventListener('keydown', onMapPinsNodeEnterPress);
 
 mapCardsNode.addEventListener('click', onMapCardsNodeClick);
 mapCardsNode.addEventListener('keydown', onMapCardsNodeEnterPress);
+
+
+var initInputsSelectsAttributes = function () {
+
+  // Добавление данных в форму ввода
+
+  noticeFormTitleInput.required = true;
+  noticeFormTitleInput.maxLength = TITLE_INPUT_MAX_LENGTH;
+
+  noticeFormAddressInput.required = true;
+  noticeFormAddressInput.readOnly = true;
+
+  noticeFormPriceInput.required = true;
+  noticeFormPriceInput.min = PRICE_INPUT_MIN_DEFAULT;
+  noticeFormPriceInput.max = PRICE_INPUT_MAX_VALUE;
+
+  //  Количество комнат связано с количеством гостей
+
+  noticeFormCapacitySelect.value = noticeFormRoomstSelect.value;
+};
+
+var onNoticeFormTimeInSelectChange = function () {
+  noticeFormTimeOutSelect.value = noticeFormTimeInSelect.value;
+};
+
+var onNoticeFormTimeOutSelectChange = function () {
+  noticeFormTimeInSelect.value = noticeFormTimeOutSelect.value;
+};
+
+var onNoticeFormRoomstSelectChange = function () {
+
+  //  Обработка неподходящих данных
+
+  if (noticeFormRoomstSelect.value === '100') {
+    noticeFormCapacitySelect.value = 0;
+  } else {
+    noticeFormCapacitySelect.value = noticeFormRoomstSelect.value;
+  }
+};
+
+var onNoticeFormTypeSelectChange = function (evt) {
+  var selectedValue = evt.target.value;
+
+  priceInputMin = PRICE_INPUT_MIN_TYPES[selectedValue];
+};
+
+var onNoticeFormPriceInputInvalid = function (evt) {
+  var target = evt.target;
+
+  if (target.value < priceInputMin) {
+    target.setCustomValidity('Минимальная цена = ' + priceInputMin);
+  } else {
+    target.setCustomValidity('');
+  }
+};
+
+var onNoticeFormTitleInputInvalid = function (evt) {
+
+  //  Edge
+
+  var target = evt.target;
+
+  if (target.value.length < TITLE_INPUT_MIN_LENGTH) {
+    target.setCustomValidity('Имя должно состоять минимум из ' + TITLE_INPUT_MIN_LENGTH + ' символов');
+  } else if (noticeFormTitleInput.validity.tooLong) {
+    noticeFormTitleInput.setCustomValidity('Имя не должно превышать ' + TITLE_INPUT_MAX_LENGTH + ' символов');
+  } else if (noticeFormTitleInput.validity.valueMissing) {
+    noticeFormTitleInput.setCustomValidity('Обязательное поле');
+  } else {
+    noticeFormTitleInput.setCustomValidity('');
+  }
+};
+
+disableNoticeForm();
+
+noticeFormTimeInSelect.addEventListener('change', onNoticeFormTimeInSelectChange);
+noticeFormTimeOutSelect.addEventListener('change', onNoticeFormTimeOutSelectChange);
+noticeFormTypeSelect.addEventListener('change', onNoticeFormTypeSelectChange);
+noticeFormRoomstSelect.addEventListener('change', onNoticeFormRoomstSelectChange);
+
+noticeFormPriceInput.addEventListener('invalid', onNoticeFormPriceInputInvalid);
+noticeFormTitleInput.addEventListener('invalid', onNoticeFormTitleInputInvalid);
